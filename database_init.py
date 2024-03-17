75e57f68658b5cd9402ec6ae02d16689d1291c02
Tarasidze <dtar84@ukr.net>
2024-03-17 21:45:42 +0200

    add database init

---
+++ database_init.py
@@ -0,0 +1,23 @@
+import os
+from dotenv import load_dotenv
+
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+
+load_dotenv()
+
+pg_db_name = os.getenv("POSTGRES_DB")
+pg_user = os.getenv("POSTGRES_USER")
+pg_pass = os.getenv("POSTGRES_PASSWORD")
+pg_host = os.getenv("POSTGRES_HOST")
+pg_port = os.getenv("POSTGRES_PORT")
+
+URL_DATABASE = f"postgresql://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_db_name}"
+
+engine = create_engine(URL_DATABASE)
+SessionLocal = sessionmaker(bind=engine)
+
+Base = declarative_base()
+session_local = SessionLocal()
